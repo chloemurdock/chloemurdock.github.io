d81e6262f825b2fffcb15f954c17bf69a10e6761
LukasMurdock <lukas.murdock@gmail.com>
2019-10-17 12:33:46 -0400

    bottom nav updates

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
 
-  $(document).ready(function() {
-    $("[href]").each(function() {
-        if (this.href == window.location.href) {
-            $(this).addClass("current");
-        }
-    });
+
+$(".bottomnav a[href]").each(function() {
+    if (this.href == document.URL) {
+        $(this).addClass("current");
+    }
 });
+
+
+/*document.querySelectorAll('a[href="'+document.URL+'"]').forEach(function(elem){elem.className += ' current'});
+
+var getURL = window.location;
+var baseURL = getURL .protocol + "//" + getURL.host + "/" + getURL.pathname.split('/')[1];
+
+$(".bottomnav a").each(function(){
+  window.console&&console.log(this);
+  if(baseURL + $(this).attr('href') == ("/"+getURL.pathname.split('/')[1]) || ($(this).attr('href') == '/' && document.URL == baseURL)){
+      $(this).css("background-color", "red");
+      window.console&&console.log(this);
+      window.console&&console.log(baseURL + $(this).attr('href'));
+      
+      
+  }
+});
+*/
 
   'use strict';
 
